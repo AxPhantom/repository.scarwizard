6effcbd1d790d0c649e034fc8c9b1517b451d71f
AxPhantom <58276215+AxPhantom@users.noreply.github.com>
2024-11-22 13:16:29 +0000

    Overhaul 2.0

--- matrix/plugin.program.scarwizard/uservar.py
+++ matrix/plugin.program.scarwizard/uservar.py
@@ -17,7 +17,7 @@ ADDONTITLE = '[COLOR orange][B]Scar[/B][/COLOR]Wizard'
 BUILDERNAME = 'Macaulay'
 EXCLUDES = [ADDON_ID, 'repository.scarwizard']
 # Text File with build info in it. Please read https://github.com/a4k-openproject/plugin.program.openwizard/wiki/Installing-Builds
-BUILDFILE = 'https://raw.githubusercontent.com/AxPhantom/repository.scarwizard/master/builds.txt'
+BUILDFILE = 'https://raw.githubusercontent.com/AxPhantom/repository.scarwizard/master/builds.txt '
 # How often you would like it to check for build updates in days
 # 0 being every startup of kodi
 UPDATECHECK = 0
@@ -77,7 +77,7 @@ THEME5 = u'[COLOR {color1}]Current Theme:[/COLOR] [COLOR {color2}]{{}}[/COLOR]'.
 # Enable 'Contact' menu item 'Yes' hide or 'No' dont hide
 HIDECONTACT = 'No'
 # You can add \n to do line breaks
-CONTACT = 'Thank you for choosing ScarWizard.\n\nContact us on Github at http://www.github.com/AxPhantom/repository.scar'
+CONTACT = 'Thank you for choosing ScarWizard.\n\nContact us on Github at http://www.github.com/AxPhantom/repository.scar '
 # Images used for the contact window.  http:// for default icon and fanart
 CONTACTICON = os.path.join(ART, 'qricon.png')
 CONTACTFANART = 'http://'
@@ -98,9 +98,9 @@ AUTOINSTALL = 'Yes'
 # Addon ID for the repository
 REPOID = 'repository.scarwizard'
 # Url to Addons.xml file in your repo folder(this is so we can get the latest version)
-REPOADDONXML = 'https://raw.githubusercontent.com/AxPhantom/repository.scarwizard/master/repo/zips/addons.xml'
+REPOADDONXML = 'https://raw.githubusercontent.com/AxPhantom/repository.scarwizard/master/repo/zips/addons.xml '
 # Url to folder zip is located in
-REPOZIPURL = 'https://raw.githubusercontent.com/AxPhantom/repository.scarwizard/master/'
+REPOZIPURL = 'https://raw.githubusercontent.com/AxPhantom/repository.scarwizard/master/ '
 #########################################################
 
 #########################################################
